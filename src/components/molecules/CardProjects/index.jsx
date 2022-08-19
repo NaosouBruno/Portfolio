868a4a940ee68b106a696e0ac6e148e0f239404f
Bruno Silva <brunoowap@gmail.com>
2022-08-19 11:50:13 -0400

    Deixando o componente de projetos reutilizavel

--- src/components/molecules/CardProjects/index.jsx
+++ src/components/molecules/CardProjects/index.jsx
@@ -1,24 +1,38 @@
 import "./cardProjects.scss";
-/* import food from "../../../assets/foodland.png"; */
-function CardProjects() {
+import { GoMarkGithub } from "react-icons/go";
+import { IconContext } from "react-icons";
+function CardProjects(props) {
   return (
     <div className="container">
-      {/*  <div className="container__card">
-        <img src={food} alt="teste" className="container__teste" />
+      <div className="container__card">
+        <img
+          src={props.projectLogo}
+          alt={props.projectLogoAlt}
+          className="container__imgProjeto"
+        />
         <div className="container__content">
-          <h3 className="container__contentTitle">Card One</h3>
-          <p className="container__contentDesc">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non,
-            obcaecati. Alias vitae rerum enim temporibus. Aliquid possimus
-            mollitia perferendis quo. Doloribus laboriosam dignissimos enim quos
-            est, doloremque animi vero harum.
-          </p>
-          <a href="#" className="container__ContentLink">
-            Read More
-          </a>
+          <h3 className="container__contentTitle">{props.projectTitle}</h3>
+          <p className="container__contentDesc">{props.projectDesc}</p>
+          <IconContext.Provider
+            value={{
+              size: "2em",
+            }}
+          >
+            <button className="sociaisGroup__socialButton sociaisGroup__socialButton--projectButton">
+              <a href={props.projectUrl}>
+                <GoMarkGithub
+                  className="sociais--icon"
+                  color="#060606"
+                  title="Github link"
+                />
+              </a>
+            </button>
+          </IconContext.Provider>
         </div>
       </div>
-      <div className="container__card">
+
+      {/* fim */}
+      {/* <div className="container__card">
         <div className="container__content">
           <h3 className="container__contentTitle">Card Two</h3>
           <p className="container__contentDesc">
@@ -46,14 +60,14 @@ function CardProjects() {
           </a>
         </div>
       </div> */}
-      <div className="containerCard">
+      {/* <div className="containerCard">
         <div className="containerCard__face containerCard__face--front">
           <h2>Front</h2>
         </div>
         <div className="containerCard__face containerCard__face--back">
           <h2>Back</h2>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/components/molecules/Projects/index.jsx
+++ src/components/molecules/Projects/index.jsx
@@ -1,9 +1,19 @@
 import "./projects.scss";
 import { CardProjects } from "../index";
+import food from "../../../assets/foodland.png";
 function Projects() {
   return (
     <section id="projetos" className="projects">
-      <CardProjects />
+      <CardProjects
+        projectLogo={food}
+        projectLogoAlt="Foodieland logo"
+        projectTitle="Foodieland"
+        projectDesc="Projeto proposto no bootcamp da Valtech, desenvolvido 
+        em grupo. Consiste na criação de blog de receitas, nesse projeto 
+        utilizamos de conceitos como Atomic Design, BEM, Mobile First. Além de 
+        tecnologias como React, React Router, Redux e SASS"
+        projectUrl="https://github.com/NaosouBruno/V-Food"
+      />
     </section>
   );
 }
