309990c95c9288f8fb225d4d5834467ddb856e3d
Bruno Silva <brunoowap@gmail.com>
2022-08-24 16:44:35 -0400

    adicionando variaveis globais no componente cardProjects

--- src/components/molecules/CardProjects/index.jsx
+++ src/components/molecules/CardProjects/index.jsx
@@ -30,44 +30,6 @@ function CardProjects(props) {
           </IconContext.Provider>
         </div>
       </div>
-
-      {/* fim */}
-      {/* <div className="container__card">
-        <div className="container__content">
-          <h3 className="container__contentTitle">Card Two</h3>
-          <p className="container__contentDesc">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non,
-            obcaecati. Alias vitae rerum enim temporibus. Aliquid possimus
-            mollitia perferendis quo. Doloribus laboriosam dignissimos enim quos
-            est, doloremque animi vero harum.
-          </p>
-          <a href="#" className="container__contentLink">
-            Read More
-          </a>
-        </div>
-      </div>
-      <div className="container__card">
-        <div className="container__content">
-          <h3 className="container__contentTitle">Card Tree</h3>
-          <p className="container__contentDesc">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non,
-            obcaecati. Alias vitae rerum enim temporibus. Aliquid possimus
-            mollitia perferendis quo. Doloribus laboriosam dignissimos enim quos
-            est, doloremque animi vero harum.
-          </p>
-          <a href="#" className="container__contentLink">
-            Read More
-          </a>
-        </div>
-      </div> */}
-      {/* <div className="containerCard">
-        <div className="containerCard__face containerCard__face--front">
-          <h2>Front</h2>
-        </div>
-        <div className="containerCard__face containerCard__face--back">
-          <h2>Back</h2>
-        </div>
-      </div> */}
     </div>
   );
 }
